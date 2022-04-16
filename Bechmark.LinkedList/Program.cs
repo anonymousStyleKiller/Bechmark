30d2d8de2583313a089747673b2e28e95ee04a14
Anton Kharchenko <gumb1t97@gmail.com>
2022-04-16 12:50:56 +0300

    :rocket: feature(Add Last): Test adding of item  to List and LinkedList
    List > LinkedList

--- Bechmark.LinkedList/Program.cs
+++ Bechmark.LinkedList/Program.cs
@@ -1,3 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run<LLBenchmark>();
+
+[MemoryDiagnoser]
+public class LLBenchmark
+{
+    [Params(1000, 10_000)] public int Size;
+    
+    [Benchmark(Baseline = true)]
+    public void ListFill()
+    {
+        var list = new List<int>();
+        for (var i = 0; i < Size; i++)
+        {
+            list.Add(i);
+        }
+    }
+    
+    [Benchmark]
+    public void LinkedListFill()
+    {
+        var list = new LinkedList<int>();
+        for (var i = 0; i < Size; i++)
+        {
+            list.AddLast(i);
+        }
+    }
+}
